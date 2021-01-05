12fe21a5dc6ea75ccfc5bc344250efe2e0aab091
eshekak <olenev.biz@gmail.com>
2021-01-05 20:21:59 +0300

    feat/basic-handlers: Added canvas

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -3,6 +3,7 @@ package handlers
 import (
         "net/http"
         "net/http/httptest"
+	"strings"
 	"testing"
 	_ "github.com/eshekak/go-ping-pong/testing"
 )
@@ -18,8 +19,22 @@ func TestMainPageHandler(t *testing.T) {
 	handler := http.HandlerFunc(MainPageHandler)
 	handler.ServeHTTP(rr, req)
 
-	expected := "<html>\n</html>\n"
-	if rr.Body.String() != expected {
-		t.Errorf("Got %v want %v", rr.Body.String(), expected)
+	got := removeAllSpaces(rr.Body.String())
+	expected := removeAllSpaces(`
+	<html>
+		<head></head>
+		<body>
+			<canvas>Classic 2D Ping Pong</canvas>
+		</body>
+	</html>`)
+
+	if got != expected {
+		t.Errorf("Got %v want %v", got, expected)
 	}
+}
+
+func removeAllSpaces(str string) string {
+	str_without_new_lines := strings.ReplaceAll(str, "\n", "")
+
+	return strings.ReplaceAll(str_without_new_lines, "\t", "")
 }
