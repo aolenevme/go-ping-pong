91b7a2d50f628fdfe666dd8ffd06a2e1c5565a7f
eshekak <olenev.biz@gmail.com>
2021-01-07 01:20:14 +0300

    feat/basic-handlers: Made paddle and ball animation. Dirty version

--- client/client.go
+++ client/client.go
@@ -2,15 +2,61 @@ package main
 
 import "syscall/js"
 
-func main() {
-	math := js.Global().Get("Math")
-	document := js.Global().Get("document")
-	canvas := document.Call("getElementById", "canvas")
-	ctx := canvas.Call("getContext", "2d")
+var (
+	math js.Value = js.Global().Get("Math")
+	document js.Value = js.Global().Get("document")
+	canvas js.Value = document.Call("getElementById", "canvas")
+	ctx js.Value = canvas.Call("getContext", "2d")
+	canvasWidth int = canvas.Get("width").Int()
+	canvasHeight int = canvas.Get("height").Int()
+	ballRadius int = 10
+	x int =  canvasWidth / 2
+	y int = canvasHeight - 30
+	dx int = 2
+	dy int = -2
+	paddleWidth int = 75
+	paddleHeight int = 10
+	paddleX int = (canvasWidth - paddleWidth) / 2
+	mainColor string = "#0095DD"
+	isDone = make(chan bool)
+)
 
+func main() {
+	js.Global().Call("setInterval", js.FuncOf(draw), "16")
+	<-isDone
+}
+
+func draw(this js.Value, args []js.Value) interface{} {
+	ctx.Call("clearRect", 0, 0, canvasWidth, canvasHeight)
+	drawBall()
+	drawPaddle()
+
+	if x + dx > canvasWidth - ballRadius || x + dx < ballRadius {
+		dx = -dx
+	}
+
+	if y + dy > canvasHeight - ballRadius || y + dy < ballRadius {
+		dy = -dy
+	}
+
+	x += dx
+	y += dy
+
+	return nil
+}
+
+func drawBall() {
 	ctx.Call("beginPath")
-	ctx.Call("arc", 50, 50, 20, 0, math.Get("PI").Int() * 2)
-	ctx.Set("fillStyle", "#0095DD")
+	ctx.Call("arc", x, y, ballRadius, 0 ,math.Get("PI").Int() * 2)
+	ctx.Set("fillStyle", mainColor)
+	ctx.Call("fill")
+	ctx.Call("closePath")
+}
+
+func drawPaddle() {
+	ctx.Call("beginPath")
+	ctx.Call("rect", paddleX, canvasHeight - paddleHeight, paddleWidth, paddleHeight)
+	ctx.Set("fillStyle", mainColor)
 	ctx.Call("fill")
 	ctx.Call("closePath")
 }
