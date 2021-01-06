a7438f4205befd1a8379fd2563a074bf48f309f5
eshekak <olenev.biz@gmail.com>
2021-01-06 23:39:18 +0300

    feat/basic-handlers: Made something to draw on canvas

--- client/client.go
+++ client/client.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import "syscall/js"
 
 func main() {
-	fmt.Println("Hello, WebAssembly!")
+	math := js.Global().Get("Math")
+	document := js.Global().Get("document")
+	canvas := document.Call("getElementById", "canvas")
+	ctx := canvas.Call("getContext", "2d")
+
+	ctx.Call("beginPath")
+	ctx.Call("arc", 50, 50, 20, 0, math.Get("PI").Int() * 2)
+	ctx.Set("fillStyle", "#0095DD")
+	ctx.Call("fill")
+	ctx.Call("closePath")
 }
