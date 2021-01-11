d4156a63a3bc2326d09bf242ee184469f9b7b27c
eshekak <olenev.biz@gmail.com>
2021-01-11 15:57:20 +0300

    feat/basic-single-animation: Formatted code

--- client/client.go
+++ client/client.go
@@ -3,25 +3,25 @@ package main
 import "syscall/js"
 
 var (
-	math js.Value = js.Global().Get("Math")
-	document js.Value = js.Global().Get("document")
-	canvas js.Value = document.Call("getElementById", "canvas")
-	ctx js.Value = canvas.Call("getContext", "2d")
-	interval js.Value = js.Null()
-	canvasWidth int = canvas.Get("width").Int()
-	canvasHeight int = canvas.Get("height").Int()
-	ballRadius int = 10
-	x int =  canvasWidth / 2
-	y int = canvasHeight - 30
-	dx int = 2
-	dy int = -2
-	paddleWidth int = 75
-	paddleHeight int = 10
-	paddleX int = (canvasWidth - paddleWidth) / 2
-	mainColor string = "#0095DD"
-	rightPressed bool = false
-	leftPressed bool = false
-	isDone = make(chan bool)
+	math         js.Value = js.Global().Get("Math")
+	document     js.Value = js.Global().Get("document")
+	canvas       js.Value = document.Call("getElementById", "canvas")
+	ctx          js.Value = canvas.Call("getContext", "2d")
+	interval     js.Value = js.Null()
+	canvasWidth  int      = canvas.Get("width").Int()
+	canvasHeight int      = canvas.Get("height").Int()
+	ballRadius   int      = 10
+	x            int      = canvasWidth / 2
+	y            int      = canvasHeight - 30
+	dx           int      = 2
+	dy           int      = -2
+	paddleWidth  int      = 75
+	paddleHeight int      = 10
+	paddleX      int      = (canvasWidth - paddleWidth) / 2
+	mainColor    string   = "#0095DD"
+	rightPressed bool     = false
+	leftPressed  bool     = false
+	isDone                = make(chan bool)
 )
 
 func main() {
@@ -62,14 +62,14 @@ func draw(this js.Value, args []js.Value) interface{} {
 	drawBall()
 	drawPaddle()
 
-	if x + dx > canvasWidth - ballRadius || x + dx < ballRadius {
+	if x+dx > canvasWidth-ballRadius || x+dx < ballRadius {
 		dx = -dx
 	}
 
-	if y + dy < ballRadius {
+	if y+dy < ballRadius {
 		dy = -dy
-	} else if y + dy > canvasHeight - ballRadius {
-		if x > paddleX && x < paddleX + paddleWidth {
+	} else if y+dy > canvasHeight-ballRadius {
+		if x > paddleX && x < paddleX+paddleWidth {
 			dy = -dy
 		} else {
 			js.Global().Call("alert", "GAME OVER")
@@ -78,7 +78,7 @@ func draw(this js.Value, args []js.Value) interface{} {
 		}
 	}
 
-	if rightPressed && paddleX < canvasWidth - paddleWidth {
+	if rightPressed && paddleX < canvasWidth-paddleWidth {
 		paddleX += 7
 	} else if leftPressed && paddleX > 0 {
 		paddleX -= 7
@@ -92,7 +92,7 @@ func draw(this js.Value, args []js.Value) interface{} {
 
 func drawBall() {
 	ctx.Call("beginPath")
-	ctx.Call("arc", x, y, ballRadius, 0 , math.Get("PI").Int() * 2)
+	ctx.Call("arc", x, y, ballRadius, 0, math.Get("PI").Int()*2)
 	ctx.Set("fillStyle", mainColor)
 	ctx.Call("fill")
 	ctx.Call("closePath")
@@ -100,7 +100,7 @@ func drawBall() {
 
 func drawPaddle() {
 	ctx.Call("beginPath")
-	ctx.Call("rect", paddleX, canvasHeight - paddleHeight, paddleWidth, paddleHeight)
+	ctx.Call("rect", paddleX, canvasHeight-paddleHeight, paddleWidth, paddleHeight)
 	ctx.Set("fillStyle", mainColor)
 	ctx.Call("fill")
 	ctx.Call("closePath")
