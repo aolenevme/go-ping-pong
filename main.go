daa4a1a8dda9a7c59d0b4e30511c53dda1dad253
eshekak <olenev.biz@gmail.com>
2021-01-12 23:53:53 +0300

    feat/interactive-via-websocket: Added http2

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	http.ListenAndServe(":8080", http.FileServer(http.Dir("./static")))
+	if err := http.ListenAndServeTLS(":8080", "security/cert.pem", "security/cert.key", http.FileServer(http.Dir("./static"))); err != nil {
+		log.Fatalf("%v", err)
+	}
 }
