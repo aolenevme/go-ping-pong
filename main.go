814bf2a68342789a738d9d9d3cbc152eb73d7187
eshekak <olenev.biz@gmail.com>
2021-01-13 21:49:20 +0300

    feat/interactive-via-websocket: Added gzip

--- main.go
+++ main.go
@@ -1,12 +1,36 @@
 package main
 
-import "net/http"
+import (
+	"compress/gzip"
+	"io"
+	"net/http"
+)
 
-// TODO
-// 1. Serve with cache
-// 2. Compress with gzip
-// 3. Make it work with tinygo
+type gzipResponseWriter struct {
+    io.Writer
+    http.ResponseWriter
+}
+
+func (w *gzipResponseWriter) Write(b []byte) (int, error) {
+    return w.Writer.Write(b)
+}
 
 func main() {
-	_ = http.ListenAndServeTLS(":8080", "security/cert.pem", "security/cert.key", http.FileServer(http.Dir("./static")))
+	fs := http.FileServer(http.Dir("./static"))
+
+	_ = http.ListenAndServeTLS(":8080", "security/cert.pem", "security/cert.key", middleware(fs))
 }
+
+func middleware(h http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	    w.Header().Set("Cache-Control", "max-age=2592000")
+	    w.Header().Set("Content-Encoding", "gzip")
+
+	    gzw := gzip.NewWriter(w)
+	    defer gzw.Close()
+
+	    h.ServeHTTP(&gzipResponseWriter{ResponseWriter: w, Writer: gzw}, r)
+    })
+}
+
+
