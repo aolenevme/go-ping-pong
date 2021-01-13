6b246018193a1da16a0b971db242fc1b774134a6
eshekak <olenev.biz@gmail.com>
2021-01-13 22:02:31 +0300

    feat/interactive-via-websocket: Refactored gzip

--- main.go
+++ main.go
@@ -6,15 +6,6 @@ import (
 	"net/http"
 )
 
-type gzipResponseWriter struct {
-    io.Writer
-    http.ResponseWriter
-}
-
-func (w *gzipResponseWriter) Write(b []byte) (int, error) {
-    return w.Writer.Write(b)
-}
-
 func main() {
 	fs := http.FileServer(http.Dir("./static"))
 
@@ -22,15 +13,22 @@ func main() {
 }
 
 func middleware(h http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	    w.Header().Set("Cache-Control", "max-age=2592000")
-	    w.Header().Set("Content-Encoding", "gzip")
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "max-age=2592000")
+		w.Header().Set("Content-Encoding", "gzip")
 
-	    gzw := gzip.NewWriter(w)
-	    defer gzw.Close()
+		gzw := gzip.NewWriter(w)
+		defer gzw.Close()
 
-	    h.ServeHTTP(&gzipResponseWriter{ResponseWriter: w, Writer: gzw}, r)
-    })
+		h.ServeHTTP(gzipResponseWriter{ResponseWriter: w, Writer: gzw}, r)
+	})
 }
 
+type gzipResponseWriter struct {
+	io.Writer
+	http.ResponseWriter
+}
 
+func (w gzipResponseWriter) Write(b []byte) (int, error) {
+	return w.Writer.Write(b)
+}
