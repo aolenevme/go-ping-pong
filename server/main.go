5d5a04ed01bc8387d6b26b966cc4f33589437b72
eshekak <olenev.biz@gmail.com>
2021-01-19 17:46:27 +0300

    feat/interactive-via-websocket: Managed to change the status

--- server/main.go
+++ server/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strconv"
 	"time"
 )
 
@@ -15,6 +14,8 @@ import (
 // 	2.1. method send -- ballX, ballY, enemyX, enemyY
 // 	2.2. method accept -- clientX, clientY
 
+var gameStatus = "WAITING"
+
 var game []Competitor
 
 type Competitor struct {
@@ -22,6 +23,34 @@ type Competitor struct {
 	r *http.Request
 	paddleX int
 	paddleY int
+}
+
+func sseHandshake(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Content-Type", "text/event-stream")
+
+	if len(game) < 1 {
+		game = append(game, Competitor{w, r, 0, 0})
+	} else {
+		gameStatus = "IN_GAME"
+	}
+
+	for {
+		fmt.Fprintf(w, "data: %s\n\n", gameStatus)
+		w.(http.Flusher).Flush()
+		time.Sleep(time.Second)
+	}
+}
+
+func sseClientPosition(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "PUT")
+		// 1. Есть игра
+		// 2. Грузится игра. Игра инициализирует handshake: /api/v1/sse. Сервер создает Competitor. В структуру Game добавляется competitor. Competitor сохраняет w и r в своей структуре. Возвращает статус WAITING_COMPETITOR игра, которая рисует соответствующий интерфейс
+		// 3. Грузится второй соперник. Все то же самое, что и в пункте 2, однако возвращается статус IN_GAME обоим конкурентам
+		// 4. Сервер начинает слать обоим конкурентам положение мяча и положение конкурентов из Competitor.paddleX и Competitor.paddleY
+		// 5. Сервер начинает принимать положения конкурентов и записывать их в Competitor.paddleX и Competitor.paddleY
+		// 6. Когда шарик улетел за границу одного из игроков, отправляется статус YOU_LOST или YOU_WON - и отрисовывается соответсвующий UI с предложением повторить. Происходит обнуление структур Competitor
+		// 7. После клика на кнопки Повторить все повторяется с пункта (1) 
 }
 
 func main() {
@@ -41,34 +70,4 @@ func main() {
 	http.Handle("/", gzipMiddleware(cacheControlMiddleware(http.FileServer(http.Dir("./static")))))
 
 	_ = http.ListenAndServeTLS(":8080", "security/cert.pem", "security/cert.key", nil)
-}
-
-func sseHandshake(w http.ResponseWriter, r *http.Request) {
-	lastEventId := 1
-
-	w.Header().Set("Connection", "keep-alive")
-	w.Header().Set("Content-Type", "text/event-stream")
-	w.Header().Set("Last-Event-ID", strconv.Itoa(lastEventId))
-
-	if len(game) < 2 {
-		game = append(game, Competitor{w, r, 0, 0})
-	}
-
-	for {
-		fmt.Fprintf(w, "id: %d\ndata: %+v\n\n", lastEventId, game)
-		w.(http.Flusher).Flush()
-		time.Sleep(10 * time.Millisecond)
-		lastEventId++
-	}
-}
-
-func sseClientPosition(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "PUT")
-		// 1. Есть игра
-		// 2. Грузится игра. Игра инициализирует handshake: /api/v1/sse. Сервер создает Competitor. В структуру Game добавляется competitor. Competitor сохраняет w и r в своей структуре. Возвращает статус WAITING_COMPETITOR игра, которая рисует соответствующий интерфейс
-		// 3. Грузится второй соперник. Все то же самое, что и в пункте 2, однако возвращается статус IN_GAME обоим конкурентам
-		// 4. Сервер начинает слать обоим конкурентам положение мяча и положение конкурентов из Competitor.paddleX и Competitor.paddleY
-		// 5. Сервер начинает принимать положения конкурентов и записывать их в Competitor.paddleX и Competitor.paddleY
-		// 6. Когда шарик улетел за границу одного из игроков, отправляется статус YOU_LOST или YOU_WON - и отрисовывается соответсвующий UI с предложением повторить. Происходит обнуление структур Competitor
-		// 7. После клика на кнопки Повторить все повторяется с пункта (1) 
 }
