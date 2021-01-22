00807e2228f1e51634025b70bd24441c6d1c775a
eshekak <olenev.biz@gmail.com>
2021-01-22 14:43:33 +0300

    feat/interactive-via-websocket: Done with SetCookie

--- server/main.go
+++ server/main.go
@@ -36,27 +36,18 @@ const (
 )
 
 var (
-	activeClients = 0
-	ballDX        = 1
-	ballDY        = -1
-	game          Game
+	ballDX = 1
+	ballDY = -1
+	game   Game
 )
 
 func sseSendInformation(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Connection", "keep-alive")
 	w.Header().Set("Content-Type", "text/event-stream")
 
-	activeClients++
-
-	if activeClients == 2 {
-		game.Status = InGame
-	} else if activeClients < 2 {
-		game.Status = WaitingCompetitor
-	}
-
 	go func() {
 		<-r.Context().Done()
-		activeClients--
+		// чекнуть куки и обнулить нужного юзера
 	}()
 
 	for {
@@ -149,7 +140,7 @@ func main() {
 
 	http.Handle("/api/v1/sse", sse)
 
-	http.Handle("/", gzipMiddleware(cacheControlMiddleware(http.FileServer(http.Dir("./static")))))
+	http.Handle("/", setCookieMiddleware(gzipMiddleware(cacheControlMiddleware(http.FileServer(http.Dir("./static"))))))
 
 	_ = http.ListenAndServeTLS(":8080", "security/cert.pem", "security/cert.key", nil)
 }
--- server/middlewares.go
+++ server/middlewares.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"compress/gzip"
+	"fmt"
 	"io"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 type gzipResponseWriter struct {
@@ -29,6 +32,18 @@ func gzipMiddleware(next http.Handler) http.Handler {
 func cacheControlMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Cache-Control", "max-age=3600")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func setCookieMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" {
+			rand.Seed(time.Now().UnixNano())
+			setCookieValue := fmt.Sprintf("client-id=%d; Secure; HttpOnly; SameSite=Strict", rand.Int())
+			w.Header().Set("Set-Cookie", setCookieValue)
+		}
+
 		next.ServeHTTP(w, r)
 	})
 }
